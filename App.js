b2e135a006b1a6fe67cc957d0a05d7e3b1e7b234
fifhrezky <you@105841102721.student.unismuh.ac.id>
2024-05-20 14:43:25 +0800

    setengah jadi

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
-import { StyleSheet, Text, View, Image,Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={{flex: 1,}}>
-      <View style= {{flex:1, flexDirection: 'row'}}>
-      <View style= {{flex:1, flexDirection: 'column'}}>
-      <View style= {{flex: 1, backgroundColor: 'black'}}></View>
-      <View style= {{flex: 1, backgroundColor: 'pink'}}></View>
-    </View>
-    <View style={{flex:1, flexDirection: 'column'}}>
-      <View style= {{flex: 1, flexDirection: 'row'}}>
-      <View style={{flex: 1, backgroundColor: 'blue'}}></View>
-      <View style= {{flex: 1, backgroundColor: 'green'}}></View>
-    </View>
-    </View>
-    </View>
-    <View style={{flex: 1, backgroundColor: 'yellow'}}></View>
-    </View>
+    <View style={{
+      flex: 1,
+      backgroundColor: 'white',
+      justifyContent: 'center', //kolom
+      alignItems: 'flex-end', //baris
+      flexDirection: 'row',
+      marginBottom: 100
+    }}>
+      <View style={{
+        backgroundColor: 'red',
+        width: 150,
+        height: 80,
+        borderRadius: 10,
+        marginRight: 10,
+      }}>
+        <Text style={{
+          color: 'white',
+          textAlign: 'center',
+          lineHeight: 80,
+          fontSize: 30,
+          fontWeight: 'bold',
+        }}>
+          Login
+        </Text>
+      </View>
+      <View style={{
+        backgroundColor: 'blue',
+        width: 150,
+        height: 80,
+        borderRadius: 10,
+        marginLeft: 10
+      }}>
+        <Text style={{
+          color: 'white',
+          textAlign: 'center',
+          lineHeight: 80,
+          fontSize: 30,
+          fontWeight: 'bold',
+        }}>
+          Register
+        </Text>
+      </View>
+    </View >
   );
 }
-
-
-
---
+++ text.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+const TextCustom = () => {
+    return(
+        <View style= {{
+            flex: 1,
+            backgroundColor: 'white',
+            justifyContent: 'flex-end',
+            alignContent: 'center'
+        }}>
+            <Text style= {{
+                fontSize: 30,
+                fontWeight: 'bold',
+                marginTop : 20
+            }}
+
+        </View>
+    )
+}
